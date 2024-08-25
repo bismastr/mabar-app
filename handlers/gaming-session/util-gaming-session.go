0a3c054d88fc1ed09358cc247b6c2ed6a1091d08
Aditya Lila <61589143+clawdius@users.noreply.github.com>
2024-08-25 16:18:44 +0700

    split functions

---
+++ handlers/gaming-session/util-gaming-session.go
@@ -0,0 +1,27 @@
+package gaming_session
+
+import (
+	"fmt"
+)
+
+var (
+	membersSession []string
+	mabarSession   bool
+)
+
+func GenerateMemberMention() string {
+	result := ""
+	for _, s := range membersSession {
+		result += fmt.Sprintf("<@%v>", s)
+	}
+	return result
+}
+
+func CheckJoin(userId string) bool {
+	for _, u := range membersSession {
+		if u == userId {
+			return true
+		}
+	}
+	return false
+}
