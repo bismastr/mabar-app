46648450edc38ab60e328b097f9ee960250e126d
bismastr <bismasatrian@gmail.com>
2024-11-25 02:19:35 +0700

    add json tag

--- internal/gaming_session/model.go
+++ internal/gaming_session/model.go
@@ -21,18 +21,18 @@ type JoinGamingSesionRequest struct {
 
 // GetGamingSession
 type GetGamingSessionByIdResponse struct {
-	SessionID    int64
-	IsFinish     pgtype.Bool
-	SessionEnd   pgtype.Timestamp
-	SessionStart pgtype.Timestamp
-	CreatedBy    GamingSessionUser
-	GameID       int64
+	SessionID    int64               `json:"session_id"`
+	IsFinish     pgtype.Bool         `json:"is_finish"`
+	SessionEnd   pgtype.Timestamp    `json:"session_end"`
+	SessionStart pgtype.Timestamp    `json:"session_start"`
+	CreatedBy    GamingSessionUser   `json:"created_by"`
+	GameID       int64               `json:"game_id"`
 	Users        []GamingSessionUser `json:"users"`
 }
 
 type GamingSessionUser struct {
-	UserID     pgtype.Int8
-	Username   pgtype.Text
-	AvatarUrl  pgtype.Text
-	DiscordUid pgtype.Int8
+	UserID     pgtype.Int8 `json:"user_id"`
+	Username   pgtype.Text `json:"username"`
+	AvatarUrl  pgtype.Text `json:"avatar_url"`
+	DiscordUid pgtype.Int8 `json:"discord_uid"`
 }
