55d8984846a5dc506cdadf83a261f3a01bd9be40
bismastr <bismasatrian@gmail.com>
2024-12-01 18:59:43 +0700

    fix discord_uid

--- internal/user/model.go
+++ internal/user/model.go
@@ -4,5 +4,5 @@ type User struct {
 	Name       string `json:"name"`
 	AvatarURL  string `json:"avatar_url"`
 	ID         int64  `json:"id"`
-	DiscordUID int64  `json:"discor_uid"`
+	DiscordUID int64  `json:"discord_uid"`
 }
